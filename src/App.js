756f0e4370b43f54e901f6830c21a25faf790d91
Victor Padilla <sonydev404@gmail.com>
2022-04-26 07:51:34 -0500

    Add style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="placeholder-container">
+        <h1 id="character-name-placeholder" className="character-name-placeholder"></h1>
+      </div>
+      <div className="grid">
+        <span className="asset top">
+          <span className="dot"></span>
+          <span className="dot"></span>
+          <span className="dot"></span>
+          <span className="line"></span>
+        </span>
+        <img className="logo" src="./static/images/logo@2x.png" width="280" alt="" />
+        <div className="social">
+          <a href="https://instagram.com/LeonidasEsteban">
+            <img src="./static/images/instagram.svg" alt="" />
+          </a>
+          <a href="https://facebook.com/LeonidasEsteban">
+            <img src="./static/images/facebook.svg" alt="" />
+          </a>
+          <a href="https://twitter.com/LeonidasEsteban">
+            <img src="./static/images/twitter.svg" alt="" />
+          </a>
+        </div>
+        <div className="navigation name">
+          <a href="#">Name</a>
+        </div>
+        <div id="character-name-container" className="character-name-container">
+
+        </div>
+
+        <div id="character-image-container" className="character-image-container">
+        </div>
+        <div className="navigation about">
+          <a href="#">About</a>
+        </div>
+
+
+        <div id="character-description-container" className="character-description-container">
+
+        </div>
+        <span className="asset bottom">
+          <span className="line"></span>
+          <span className="dot"></span>
+          <span className="dot"></span>
+          <span className="dot"></span>
+        </span>
+
+        <div></div>
+        <div className="learn-more">
+          <span>learn more</span>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/character.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class character extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default character;
